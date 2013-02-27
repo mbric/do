68164476d7b763a007df7d09cfa3883d7b9eaf50
mbric <mattbrickner@gmail.com>
2013-02-27 18:42:47 -0500

    removed some old comments

--- public/js/controllers.js
+++ public/js/controllers.js
@@ -17,11 +17,9 @@ function TaskCtrl($scope, sharedTaskModel) {
 }
 
 function TreeController($scope, sharedCommentModel) {
-  //$scope.test = sharedCommentModel
   $scope.tree = sharedCommentModel
   $scope.delete = function(data) {
       data.nodes = [];
-      //data.nodes.clear();
   };
   $scope.addDefault = function(data) {
       $scope.tree.push({name: "Node", nodes: []})
@@ -31,8 +29,6 @@ function TreeController($scope, sharedCommentModel) {
       var newName = data.name + '-' + post;
       data.nodes.push({name: newName,nodes: []});
   };
-  //$scope.tree = [{name: "Node", nodes: []}];
-  //$scope.tree = [{name: "Node", nodes: [{name: "Node-X1", nodes: []}, {name: "Node-X2", nodes: []}]}, {name: "Node-X", nodes: [{name: "Node-X1", nodes: []}, {name: "Node-X2", nodes: []}]}];
 }
 
 function MyCtrl1($scope, socket) {
